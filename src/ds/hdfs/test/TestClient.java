03c21142a7a23848c9bc500ce522ccc4d076d7e2
Alexander Goodkind <1017680+agoodkind@users.noreply.github.com>
2020-03-17 16:12:22 -0400

    updated

--- src/ds/hdfs/test/TestClient.java
+++ src/ds/hdfs/test/TestClient.java
@@ -1,15 +1,20 @@
 package ds.hdfs.test;
 
+import com.google.protobuf.BlockingRpcChannel;
 import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
-import ds.hdfs.test.TestRequest;
 import ds.hdfs.test.TestReply;
 
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TestClient {
     private static final Logger logger = Logger.getLogger(TestClient.class.getName());
-    private final TestService.Stub blockingStub;
+    private final TestService.BlockingStub blockingStub;
 
     /** Construct client for accessing HelloWorld server using the existing channel. */
     public TestClient(Channel channel) {
@@ -17,27 +22,60 @@ public class TestClient {
         // shut it down.
 
         // Passing Channels to code makes code easier to test and makes it easier to reuse Channels.
-        blockingStub = TestService.newBlockingStub(channel);
+        blockingStub = (TestService.BlockingStub) TestService.newBlockingStub((BlockingRpcChannel) channel);
     }
 
 
-    public void greet(String name) {
-        logger.info("Will try to greet " + name + " ...");
-        TestRequest request = TestRequest.newBuilder().setTestRequestField(name).build();
+    public void testClientMethod(String testString) {
+        logger.info("Will try to greet " + testString + " ...");
+        TestRequest request = TestRequest.newBuilder().setTestRequestField(testString).build();
         TestReply response;
         try {
-            response = blockingStub.sayHello(request);
+            response = blockingStub.testThisService(request);
         } catch (StatusRuntimeException e) {
             logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
             return;
         }
-        logger.info("Greeting: " + response.getMessage());
+        logger.info("Greeting: " + response.getTestReplyField());
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Access a service running on the local machine on port 50051
+        String target = "localhost:50051";
+        // Allow passing in the user and target strings as command line arguments
+//        if (args.length > 0) {
+//            if ("--help".equals(args[0])) {
+//                System.err.println("Usage: [name [target]]");
+//                System.err.println("");
+//                System.err.println("  name    The name you wish to be greeted by. Defaults to " + user);
+//                System.err.println("  target  The server to connect to. Defaults to " + target);
+//                System.exit(1);
+//            }
+//            user = args[0];
+//        }
+//        if (args.length > 1) {
+//            target = args[1];
+//        }
+
+        // Create a communication channel to the server, known as a Channel. Channels are thread-safe
+        // and reusable. It is common to create channels at the beginning of your application and reuse
+        // them until the application shuts down.
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build();
         try {
-            response = blockingStub.sayHelloAgain(request);
-        } catch (StatusRuntimeException e) {
-            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-            return;
+            TestClient client = new TestClient(channel);
+            client.testClientMethod("testtttttkjasldjalksjd");
+        } finally {
+            // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
+            // resources the channel should be shut down when it will no longer be used. If it may be used
+            // again leave it running.
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
         }
-        logger.info("Greeting: " + response.getMessage());
     }
 }
+
+
