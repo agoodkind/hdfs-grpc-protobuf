41c10b3d09674e3ac805e9e84549d7e0bfffe957
Alex <amg540@rutgers.edu>
2020-03-19 18:57:49 -0400

    wipppy

--- src/ds/hdfs/NameNode.java
+++ src/ds/hdfs/NameNode.java
@@ -1,18 +1,56 @@
 package ds.hdfs;
 
-import ds.hdfs.generated.BlockLocationMapping;
-import ds.hdfs.generated.FileMetadata;
-import ds.hdfs.generated.NameNodeGrpc;
-import ds.hdfs.test.TestServer;
+import ds.hdfs.generated.*;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
+
 public class NameNode extends NameNodeGrpc.NameNodeImplBase {
+
+    private static int BLOCK_SIZE;
+    private static int PORT_NUM;
+
+    public NameNode(int portNum, int blockSize) {
+        this.BLOCK_SIZE = blockSize;
+        this.PORT_NUM = portNum;
+    }
+
+    class SingleFile {
+        /**
+         * a mapping of mappings.....just think about it
+         * maps metadata about a file to the list of mappings for its blocks
+         */
+        private FileMetadata fileMetadata;
+        private BlockMetadata[] blockList;
+        private int numBlocks;
+
+
+        public SingleFile(FileMetadata fileMetadata) {
+            this.fileMetadata = fileMetadata;
+            this.numBlocks =  (int) Math.ceil((double) fileMetadata.getSize() / BLOCK_SIZE);
+            this.blockList = new BlockMetadata[numBlocks];
+        }
+
+        public void addBlock(BlockMetadata blockMetadata) {
+            this.blockList[blockMetadata.getIndex()] = blockMetadata;
+        }
+
+
+
+    }
+
+
     /**
      * NameNode implementation
      */
@@ -30,18 +68,18 @@ public class NameNode extends NameNodeGrpc.NameNodeImplBase {
 
     private Server server;
 
-    private void startServer(int port) throws IOException {
+    private void startServer() throws IOException {
 
         if (server != null) {
             throw new IllegalStateException("Already started");
         }
 
-        server = ServerBuilder.forPort(port)
-                .addService(new NameNode())
+        server = ServerBuilder.forPort(PORT_NUM)
+                .addService(new NameNode(PORT_NUM, BLOCK_SIZE))
                 .build()
                 .start();
 
-        logger.info("Server started, listening on " + port);
+        logger.info("Server started, listening on " + PORT_NUM);
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
@@ -64,11 +102,14 @@ public class NameNode extends NameNodeGrpc.NameNodeImplBase {
     }
 
     public static void main(String[] args) throws IOException {
-        // read in config file
+        // read in the two config files
         // we need port
+        // and block size
 //        String config = args[0 or 1 i cant remember];
-        int port = 50051;
-        final NameNode nameNodeServer = new NameNode();
-        nameNodeServer.startServer(port);
+        int blockSize = 64; // bytes
+        int portNum = 50051;
+
+        final NameNode nameNodeServer = new NameNode(portNum, blockSize);
+        nameNodeServer.startServer();
     }
 }
