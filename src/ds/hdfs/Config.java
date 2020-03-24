04fa5e9b9a5a7d9208d424fc183fbae25b241f4a
Alexander Goodkind <1017680+agoodkind@users.noreply.github.com>
2020-03-24 19:45:52 -0400

    config init

---
+++ src/ds/hdfs/Config.java
@@ -0,0 +1,21 @@
+package ds.hdfs;
+
+public class Config {
+    static class NNConfig {
+
+    }
+
+    static class DNConfig {
+
+    }
+
+    static class ClientConfig {
+
+    }
+
+    public static void readConfig() {
+
+    }
+
+
+}
